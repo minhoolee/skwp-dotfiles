cf338b00909f6947bc1c2eb1ea976ddbaf8e2d5e
minhoolee <minhoo2000@gmail.com>
2015-10-12 12:10:10 -0700

    Add in zsh file for environment variables

---
+++ zsh/export-variables.zsh
@@ -0,0 +1,8 @@
+# Export all the environment level variables here
+
+# Slack-workout environment variables
+export SLACK_USER_TOKEN_STRING=xoxp-12023706278-12021191235-12278603891-b3f71905d6
+export SLACK_URL_TOKEN_STRING=Ior8FXZJ99R5did7cgGphWlx
+
+# PostGreSQL path to executable
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
